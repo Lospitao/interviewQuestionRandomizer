0c398f52eb41f2267fdaadcec9217091c92d6078
Laura Lospitao <lauralospitao@gmail.com>
2021-08-13 10:57:01 +0200

    Index page > As a user, when I enter the app, I am given two options: create a new question-answear set or practice #1

---
+++ src/Controller/Index/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Index;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+            'apiHost' => $this->getParameter('api_host'),
+        ]);
+    }
+}
